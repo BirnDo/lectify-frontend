10eb961c240e570ced82effda79ca3b91ba36484
Lukas Werner <lukas.werner001@gmail.com>
2025-01-24 16:25:51 +0100

    added error handling to forward to error pages; changed fetch method to svelte on which includes automatic cookie apending

--- lectify/src/routes/history/+page.ts
+++ lectify/src/routes/history/+page.ts
@@ -1,38 +1,29 @@
+import { error } from '@sveltejs/kit';
 import type { Summary } from '../../models/Summary';
 import type { PageLoad } from './$types';
-const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
 
-export const load: PageLoad = async ({ data }) => {
-	const { token } = data;
-	console.log(token);
-	let summaries: Summary[] = [];
-	if (token) {
-		summaries = await fetchSummaries(); // preload if server cookie jwt is present
-		console.log(summaries);
+export const load: PageLoad = async ({ fetch, data }) => {
+	const res = await fetch(`/api/history`);
+
+	if (res.status === 401) error(res.status, 'Please login to view your history.');
+	else if (!res.ok) {
+		error(res.status, {
+			message: `Error loading history: ${res.statusText}`
+		});
 	}
+	const token = data.token;
+	let summaries: Summary[] = (await res.json()).map((summary: Summary) => ({
+		...summary,
+		transcriptionQuality: mapTranscriptionQuality(summary.transcriptionQuality)
+	}));
+
+	if (summaries.length === 0) error(404, 'No history for your account found.');
+
 	return {
 		token,
-		summaries,
-		fetchSummaries
+		summaries
 	};
 };
-
-async function fetchSummaries() {
-	const response = await fetch(API_ENDPOINT + '/history', {
-		method: 'GET',
-		credentials: 'include'
-	});
-	const summaries: Summary[] = await response.json();
-	console.log('Fetched summaries');
-	console.log(summaries);
-
-	summaries.forEach(
-		(summary) =>
-			(summary.transcriptionQuality = mapTranscriptionQuality(summary.transcriptionQuality))
-	);
-
-	return summaries;
-}
 
 function mapTranscriptionQuality(quality: string): Summary['transcriptionQuality'] {
 	switch (quality) {
